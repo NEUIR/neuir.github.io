0a01b024fda99bdd1548c47c2508bd4e059c27b6
xhd0728 <77320781+xhd0728@users.noreply.github.com>
2025-02-16 02:27:18 +0000

    Clear branch

--- _cite/_test_scholarly.py
+++
@@ -1,8 +0,0 @@
-from scholarly import scholarly
-
-_id = "4vrZRk0AAAAJ"
-author = scholarly.search_author_id(_id)
-pubs = scholarly.fill(author, sections=["publications"])["publications"]
-title_list = [pub["bib"]["title"] for pub in pubs]
-search_query = scholarly.search_pubs(title_list[0])
-scholarly.pprint(next(search_query))
--- course/index.md
+++
@@ -1,9 +0,0 @@
----
-title: 合作课程
-nav:
-  order: 5
----
-
-# {% include icon.html icon="fas fa-chalkboard-teacher" %}Courses
-
-{% include list.html data="courses" component="post-excerpt" %}
